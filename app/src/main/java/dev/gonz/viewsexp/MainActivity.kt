9045b2c3a5ed68afb5b06e362bcd24d71a3ee04b
Gonzalo Campos <gonzalocampose1@gmail.com>
2023-07-23 16:06:06 -0600

    Play with gravity sensor.

--- app/src/main/java/dev/gonz/viewsexp/MainActivity.kt
+++ app/src/main/java/dev/gonz/viewsexp/MainActivity.kt
@@ -1,7 +1,13 @@
 package dev.gonz.viewsexp
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import dagger.hilt.android.AndroidEntryPoint
 import dev.gonz.viewsexp.databinding.ActivityMainBinding
 import dev.gonz.viewsexp.views.MainActivityViewBinder
@@ -15,9 +21,43 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewBinder: MainActivityViewBinder
 
+    private val sensorManager by lazy { getSystemService(Context.SENSOR_SERVICE) as SensorManager }
+
+    private lateinit var gravitySensorListener: SensorEventListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinder = viewBinderFactory.create(ActivityMainBinding.inflate(layoutInflater))
+        subscribeToGravitySensor()
         setContentView(viewBinder.root)
     }
+
+    private fun subscribeToGravitySensor() {
+        val sensor: Sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
+        gravitySensorListener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent?) {
+                Log.d("Gravity Sensor Changed", event?.description()!!)
+            }
+
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+                Log.d("Gracity Accuracy Changed", accuracy.toString())
+            }
+        }
+        sensorManager.registerListener(gravitySensorListener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onStop() {
+        if (::gravitySensorListener.isInitialized) {
+            sensorManager.unregisterListener(gravitySensorListener)
+        }
+        super.onStop()
+    }
 }
+
+fun SensorEvent.description() = buildString {
+    append("SensorEvent{")
+    append("${values.string()}, $timestamp")
+    append("}")
+}
+
+fun FloatArray.string() = joinToString(separator = ", ", prefix = "[", postfix = "]")
